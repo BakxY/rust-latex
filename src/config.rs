f0055b9c3956f965f8748e4999a0acff1a741a93
Severin Sprenger <severin.sprenger@sespra.ch>
2025-04-01 13:35:34 +0200

    Finished function for getting human readable name of a template

--- src/config.rs
+++ src/config.rs
@@ -6,14 +6,27 @@
  * @author Severin Sprenger (BakxY)
  */
 
-use std::{fs, path::Path};
+use std::fs;
+use std::path::Path;
 use crate::files;
 
 pub fn get_human_readable_name(template_name: String) -> String
 {
     let base_path = files::get_template_location();
 
-    let path_to_config = Path::new(base_path.as_str()).join(template_name.clone()).join(template_name + ".conf");
+    let path_to_config = Path::new(base_path.as_str()).join(template_name.clone()).join(template_name.clone() + ".conf");
 
-    return "".to_string();
+    let config_file = fs::read_to_string(path_to_config).unwrap();
+
+    let config_lines = config_file.split("\n");
+
+    for line in config_lines {
+        let line = line.to_string();
+
+        if line.starts_with("READABLE ") {
+            return line.replace("READABLE ", "");
+        }
+    }
+
+    return template_name;
 }
